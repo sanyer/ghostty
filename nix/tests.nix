f26a6b949c58f0f7e3587a8f17997e868719abd9
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-08-22 17:05:18 -0500

    nix vm tests: sync ghostty user with other tests

--- nix/tests.nix
+++ nix/tests.nix
@@ -136,6 +136,8 @@ in {
         users.users.ghostty = {
           isNormalUser = true;
           group = "ghostty";
+          extraGroups = ["wheel"];
+          hashedPassword = "";
           packages = [
             pkgs.ghostty
           ];
