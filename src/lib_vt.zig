64f26c14d3f370b026f61c8b6c53d4bb32a61b5b
Mitchell Hashimoto <m@mitchellh.com>
2025-09-21 10:06:53 -0700

    example/zig-vt

--- src/lib_vt.zig
+++ src/lib_vt.zig
@@ -1,4 +1,13 @@
+//! This is the public API of the ghostty-vt Zig module.
+
+// The public API below reproduces a lot of terminal/main.zig but
+// is separate because (1) we need our root file to be in `src/`
+// so we can access other directories and (2) we may want to withhold
+// parts of `terminal` that are not ready for public consumption
+// or are too Ghostty-internal.
 const terminal = @import("terminal/main.zig");
+pub const Parser = terminal.Parser;
+pub const Terminal = terminal.Terminal;
 
 test {
     _ = terminal;
