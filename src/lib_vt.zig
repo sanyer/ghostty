9ba45b21639870af1f675969a67fca4a52ab9fa6
Mitchell Hashimoto <m@mitchellh.com>
2025-09-29 09:18:41 -0700

    lib-vt: fix invalid Zig forwards

--- src/lib_vt.zig
+++ src/lib_vt.zig
@@ -31,8 +31,8 @@ pub const size = terminal.size;
 pub const x11_color = terminal.x11_color;
 
 pub const Charset = terminal.Charset;
-pub const CharsetSlot = terminal.Slots;
-pub const CharsetActiveSlot = terminal.ActiveSlot;
+pub const CharsetSlot = terminal.CharsetSlot;
+pub const CharsetActiveSlot = terminal.CharsetActiveSlot;
 pub const Cell = page.Cell;
 pub const Coordinate = point.Coordinate;
 pub const CSI = Parser.Action.CSI;
