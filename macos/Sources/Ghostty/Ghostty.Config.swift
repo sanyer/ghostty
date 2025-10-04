0b14026696e0bf54d5dba8940d8ae97201ddab27
Daniel Wennberg <daniel.wennberg@gmail.com>
2025-10-04 00:04:19 -0700

    Expand `~` in `macos-custom-icon`

--- macos/Sources/Ghostty/Ghostty.Config.swift
+++ macos/Sources/Ghostty/Ghostty.Config.swift
@@ -314,17 +314,14 @@ extension Ghostty {
 
         var macosCustomIcon: String {
             #if os(macOS)
-            let homeDirURL = FileManager.default.homeDirectoryForCurrentUser
-            let ghosttyConfigIconPath = homeDirURL.appendingPathComponent(
-                ".config/ghostty/Ghostty.icns",
-                conformingTo: .fileURL).path()
-            let defaultValue = ghosttyConfigIconPath
+            let defaultValue = NSString("~/.config/ghostty/Ghostty.icns").expandingTildeInPath
             guard let config = self.config else { return defaultValue }
             var v: UnsafePointer<Int8>? = nil
             let key = "macos-custom-icon"
             guard ghostty_config_get(config, &v, key, UInt(key.count)) else { return defaultValue }
             guard let ptr = v else { return defaultValue }
-            return String(cString: ptr)
+            guard let path = NSString(utf8String: ptr) else { return defaultValue }
+            return path.expandingTildeInPath
             #else
             return ""
             #endif
