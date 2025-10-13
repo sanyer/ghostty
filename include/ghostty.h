d6607997235ad5059f3c7692df2fd2e9154127a6
Peter Guy <peter.guy@sourcegraph.com>
2025-10-13 10:59:01 -0700

    Consolidate the several ghostty_surface_inherited_config functions back into a single function with a second parameter for the source context.

--- include/ghostty.h
+++ include/ghostty.h
@@ -1042,10 +1042,7 @@ ghostty_surface_t ghostty_surface_new(ghostty_app_t,
 void ghostty_surface_free(ghostty_surface_t);
 void* ghostty_surface_userdata(ghostty_surface_t);
 ghostty_app_t ghostty_surface_app(ghostty_surface_t);
-ghostty_surface_config_s ghostty_surface_inherited_config(ghostty_surface_t); /* deprecated: use context-specific functions */
-ghostty_surface_config_s ghostty_surface_inherited_config_window(ghostty_surface_t);
-ghostty_surface_config_s ghostty_surface_inherited_config_tab(ghostty_surface_t);
-ghostty_surface_config_s ghostty_surface_inherited_config_split(ghostty_surface_t);
+ghostty_surface_config_s ghostty_surface_inherited_config(ghostty_surface_t, ghostty_surface_context_e);
 void ghostty_surface_update_config(ghostty_surface_t, ghostty_config_t);
 bool ghostty_surface_needs_confirm_quit(ghostty_surface_t);
 bool ghostty_surface_process_exited(ghostty_surface_t);
