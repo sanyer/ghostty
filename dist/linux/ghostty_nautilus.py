622d49206a27659381f8a994b792215525a80c2f
Sebastian Wiesner <sebastian@swsnr.de>
2025-12-29 17:42:19 +0100

    Remove unused imports

--- dist/linux/ghostty_nautilus.py
+++ dist/linux/ghostty_nautilus.py
@@ -17,8 +17,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from os.path import isdir
-from gi import require_version
 from gi.repository import Nautilus, GObject, Gio
 
 
