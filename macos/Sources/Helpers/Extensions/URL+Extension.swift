07abbd1e7ee1f98d40cffacf537577e0bcb3522b
Mitchell Hashimoto <m@mitchellh.com>
2026-08-27 14:57:59 -0700

    Update macos/Sources/Helpers/Extensions/URL+Extension.swift
    Co-authored-by: Lukas <134181853+bo2themax@users.noreply.github.com>

--- macos/Sources/Helpers/Extensions/URL+Extension.swift
+++ macos/Sources/Helpers/Extensions/URL+Extension.swift
@@ -3,10 +3,6 @@ import Foundation
 extension URL {
     /// The decoded path with trailing separators removed, except for the root path.
     var pathWithoutTrailingSlash: String {
-        var result = path(percentEncoded: false)
-        while result.count > 1 && result.hasSuffix("/") {
-            result.removeLast()
-        }
-        return result
+        FilePath(path(percentEncoded: false)).string
     }
 }
